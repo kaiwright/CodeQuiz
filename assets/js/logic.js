cf3ceb766a270ff2c62d3eec5c0bbc1b5f90b7a2
kaiwright <kaimatthewwright@gmail.com>
2023-11-02 16:06:24 +0000

    Added javascript files and JS code to have timer on start

---
+++ assets/js/logic.js
@@ -0,0 +1,29 @@
+var time = document.getElementById('time')
+var startButton = document.getElementById('start')
+
+// event for start button being clicked - start timer and display questions
+startButton.addEventListener("click", function() {
+    timerCountdown()
+})
+
+
+
+// timer countdown
+function timerCountdown() {
+    var timeLeft = 60;
+
+    var timeInterval = setInterval(function () {
+
+        // As long as the `timeLeft` is greater than 1
+        if (timeLeft > 1) {
+            // Set the `textContent` of `timerEl` to show the remaining seconds
+            time.textContent = timeLeft;
+            // Decrement `timeLeft` by 1
+            timeLeft--;
+        } else {
+            // Once `timeLeft` gets to 0, set `timerEl` to an empty string
+            time.textContent = '';
+            clearInterval(timeInterval);
+        }
+    }, 1000);
+}
